3c3f6f3e75c6fdd5e1c2b4cfb7a1ed724f28d8a3
James E Keenan <jkeenan@cpan.org>
2017-04-17 10:13:26 -0400

    Store add-to-index.pl in prep-perlweb.

---
+++ add-to-index.pl
@@ -0,0 +1,56 @@
+# perl
+use strict;
+use warnings;
+use 5.10.1;
+use Data::Dumper;$Data::Dumper::Indent=1;
+use Data::Dump qw( dd pp );
+use Carp;
+use Cwd;
+
+#my/home/jkeenan/gitwork/perlweb/docs/dev/perl5/news
+
+=pod
+
+    <li>
+        <a href="2012/perl-5.16.2.html">Perl 5.16.2 Released</a> -- (Nov 1, 2012)<br>
+    </li>
+
+=cut
+
+my %months = (
+    '01'    => 'Jan',
+    '02'    => 'Feb',
+    '03'    => 'Mar',
+    '04'    => 'Apr',
+    '05'    => 'May',
+    '06'    => 'Jun',
+    '07'    => 'Jul',
+    '08'    => 'Aug',
+    '09'    => 'Sep',
+    '10'    => 'Oct',
+    '11'    => 'Nov',
+    '12'    => 'Dec',
+);
+my $cwd = cwd();
+my $pvn = "$cwd/production-versions-needed.txt";
+my %releases;
+open my $IN, '<', $pvn or croak "Unable to open for reading";
+while (my $r = <$IN>) {
+	chomp $r;
+    my ($version, $date) = split(/\t/, $r);
+    next unless $date =~ m/^(\d{4})-(\d\d)-(\d\d)$/;
+    my ($year, $month, $day) = ($1,$2,$3);
+    my $d = 0+$day;
+    my $v = $version =~ s/^v//r;
+    my $li = <<END_OF_ITEM;
+    <li>
+        <a href="$year/perl-${v}.html">Perl $v Released</a> -- ($months{$month} $d, $year)<br>
+    </li>
+
+END_OF_ITEM
+    $releases{$v} = $li;
+}
+close $IN or croak "Unable to close after reading";
+for my $r ( sort { $b cmp $a  } keys %releases ) {
+    say $releases{$r};
+}
